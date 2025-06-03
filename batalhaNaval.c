55131b4a00815ed73a36d62151f987c1e1d50c4c
Humberto <76440660+deAzevedoHC@users.noreply.github.com>
2025-06-03 02:18:45 +0000

    Nível Aventureiro preferi usar uma abordagem com funções, desta forma no mesmo local que está o código que insere a posição do navio no tabuleiro, contém também a validação da posição.

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -3,51 +3,105 @@
 // Desafio Batalha Naval - MateCheck
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
+void naviohorizontal(int tabuleiro[10][10], int posicao[2], int tamanho) {
+    //verificar se a posição do navio está dentro dos limites do tabuleiro
+    if (posicao[0] < 0 || posicao[0] >= 10 || posicao[1] < 0 || posicao[1] + tamanho > 10) {
+        printf("Posição inválida para o navio horizontal.\n");
+        return;
+    }else if (tabuleiro[posicao[0]][posicao[1]] != 0) {
+        printf("Posição já ocupada por outro navio.\n");
+        return;
+    } else {
+        // Coloca o navio horizontalmente no tabuleiro
+        for (int i = 0; i < tamanho; i++) {
+            tabuleiro[posicao[0]][posicao[1] + i] = 3; // Marca a posição do navio no tabuleiro
+        }
+    }
+}
+
+void naviovertical(int tabuleiro[10][10], int posicao[2], int tamanho) {
+    //verificar se a posição do navio está dentro dos limites do tabuleiro
+    if (posicao[0] < 0 || posicao[0] + tamanho > 10 || posicao[1] < 0 || posicao[1] >= 10) {
+        printf("Posição inválida para o navio vertical.\n");
+        return;
+    }else if (tabuleiro[posicao[0]][posicao[1]] != 0) {
+        printf("Posição já ocupada por outro navio.\n");
+        return;
+    } else {
+        // Coloca o navio verticalmente no tabuleiro
+        for (int i = 0; i < tamanho; i++) {
+            tabuleiro[posicao[0] + i][posicao[1]] = 3; // Marca a posição do navio no tabuleiro
+        }
+    }
+    }
+
+void naviodiagonal_direita(int tabuleiro[10][10], int posicao[2], int tamanho) {
+    //verificar se a posição do navio está dentro dos limites do tabuleiro
+    if (posicao[0] < 0 || posicao[0] + tamanho > 10 || posicao[1] < 0 || posicao[1] + tamanho > 10) {
+        printf("Posição inválida para o navio diagonal direita.\n");
+        return;
+    }else if (tabuleiro[posicao[0]][posicao[1]] != 0) {
+        printf("Posição já ocupada por outro navio.\n");
+        return;
+    } else {
+        // Coloca o navio diagonalmente para a direita no tabuleiro
+        for (int i = 0; i < tamanho; i++) {
+            tabuleiro[posicao[0] + i][posicao[1] + i] = 3; // Marca a posição do navio no tabuleiro
+        }
+    }
+}
+
+void naviodiagonal_esquerda(int tabuleiro[10][10], int posicao[2], int tamanho) {
+    //verificar se a posição do navio está dentro dos limites do tabuleiro
+    if (posicao[0] < 0 || posicao[0] + tamanho > 10 || posicao[1] < tamanho - 1 || posicao[1] >= 10) {
+        printf("Posição inválida para o navio diagonal esquerda.\n");
+        return;
+    }else if (tabuleiro[posicao[0]][posicao[1]] != 0) {
+        printf("Posição já ocupada por outro navio.\n");
+        return;
+    } else {
+        // Coloca o navio diagonalmente para a esquerda no tabuleiro
+        for (int i = 0; i < tamanho; i++) {
+            tabuleiro[posicao[0] + i][posicao[1] - i] = 3; // Marca a posição do navio no tabuleiro
+        }
+    }
+}
+
 
 int main() {
-     int tabuleiro [10][10] = {0}; // tabuleiro inicial
+   int tabuleiro [10][10] = {0}; // tabuleiro inicial
     int navio1 [3] = {3}; //navio 1 de tamanho 3
     int navio2 [3] = {3}; // navio 2 de tamanho 3
+    int navio3 [3] = {3}; // navio 3 de tamanho 3
+    int navio4 [3] = {3}; // navio 4 de tamanho 3
     int i,j;
     int tamanho1 = sizeof(navio1) / sizeof(navio1[0]); // tamanho do navio 1
     int tamanho2 = sizeof(navio2) / sizeof(navio2[0]); // tamanho do navio 2
-    int posicaoinicial1 = 4; // posição inicial do navio 1
-    int posicaoinicial2 = 5; // posição inicial do navio 2
+    int tamanho3 = sizeof(navio3) / sizeof(navio3[0]); // tamanho do navio 3
+    int tamanho4 = sizeof(navio4) / sizeof(navio4[0]); // tamanho do navio 4    
+    int posicaoinicial1 [1][2] = {{1, 0}}; // posição inicial do navio 1
+    int posicaoinicial2 [1][2] = {{5, 6}}; // posição inicial do navio 2
+    int posicaoinicial3 [1][2] = {{7, 1}}; // posição inicial do navio 3    
+    int posicaoinicial4 [1][2] = {{1, 8}}; // posição inicial do navio 4
 
-    // verifica se a posição inicial do navio 1 é válida para que o navio esteja dentro do tabuleiro
-    if (posicaoinicial1 + tamanho1 > 10) {
-        printf("O navio 1 não está fora dos limites do tabuleiro, escolha outra posição inicial!\n");
-        return 1; // encerra o programa se a posição inicial for inválida
-    }
-    // verifica se a posição inicial do navio 2 é válida para que o navio esteja dentro do tabuleiro
-    if (posicaoinicial2 + tamanho2 > 10) {
-        printf("O navio 2 não está fora dos limites do tabuleiro, escolha outra posição inicial!\n");
-        return 1; // encerra o programa se a posição inicial for inválida
-    }
-    
-    // Posiciona navio1 na linha 2, colunas 4, 5 e 6 (horizontal)
-    for (i = 0; i < tamanho1 ; i++) {
-        tabuleiro[2][posicaoinicial1 + i] = 3; // representa navio1, sendo colocado na sua posição inicial mais tres unidades para a direita
-    }
+    //colocando navio 1 horizontal
+    naviohorizontal(tabuleiro, posicaoinicial1[0], tamanho1);
 
-    //Verificar se o navio 2 irá se sobrepor ao navio 1
-    for (i = 0; i < tamanho2; i++) {
-        if (tabuleiro[posicaoinicial2 + i][7] != 0) {
-            printf("O navio 2 não pode ser posicionado nesta nessa posição!\n");
-            return 1; // encerra o programa se houver sobreposição
-        }
-    }
+    //colocando navio 2 vertical
+    naviovertical(tabuleiro, posicaoinicial2[0], tamanho2);
 
-    // Posiciona navio2 na coluna 7, linhas 5, 6 e 7 (vertical)
-    for (i = 0; i < tamanho2; i++) {
-        tabuleiro[posicaoinicial2 + i][7] = 3; //representa navio2, sendo colocado na sua posição inicial mais tres unidades para baixo
-    }
+    //colocando navio 3 diagonal direita
+    naviodiagonal_direita(tabuleiro, posicaoinicial3[0], tamanho3);
+
+    //colocando navio 4 diagonal esquerda
+    naviodiagonal_esquerda(tabuleiro, posicaoinicial4[0], tamanho4);
+
 
     //exibir tabuleiro
     for (i = 0; i < 10; i++) {
         for ( j = 0; j < 10; j++)
     {
-        printf("%d ",tabuleiro[i][j]);
+        printf("%d  ",tabuleiro[i][j]);
     }
     printf("\n");
     }
